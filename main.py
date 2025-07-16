cd0ee79fe179cb3519eeea1db8b2b88cec746df4
Kim254 <shadykimtai254@gmail.com>
2025-07-16 19:44:40 +0300

    adjusted code to analyze the efficiency and profitability of the volatility trading strategy against a simple buy and hold strategy.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# The code below is meant to test the profitability of the volatility trading strategy in the long run over the hold and buy strategy
+# The data was obtained from
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -17,29 +19,31 @@ close_list = df["Close"].tolist()
 
 all_price_list = open_list + high_list + low_list + close_list
 
-# all_price_list = [1, 3, 2, 3, 2, 3, 1, 3]
+# all_price_list = [1, 3, 2, 3]
 # print(all_price_list)
 polished_price_list = []
 ratio_drop = 0.9
 ratio_increase = 1.1
 
+
 def list_polishing_function():
     count = 0
     for q in all_price_list:
-        polished_price_list.append(q)
-        count += 1
-        if count < len(all_price_list):
-            if all_price_list[count] > all_price_list[count - 1]:
-                while q < all_price_list[count]:
-                    q += ratio_increase - 1
-                    if q < all_price_list[count]:
-                        polished_price_list.append(q)
+        if q != "unknown":
+            polished_price_list.append(q)
+            count += 1
+            if count < len(all_price_list):
+                if all_price_list[count] > all_price_list[count - 1]:
+                    while q < all_price_list[count]:
+                        q += ratio_increase - 1
+                        if q < all_price_list[count]:
+                            polished_price_list.append(q)
 
-            elif all_price_list[count] < all_price_list[count - 1]:
-                while q > all_price_list[count]:
-                    q -= ratio_increase - 1
-                    if q > all_price_list[count]:
-                        polished_price_list.append(q)
+                elif all_price_list[count] < all_price_list[count - 1]:
+                    while q > all_price_list[count]:
+                        q -= ratio_increase - 1
+                        if q > all_price_list[count]:
+                            polished_price_list.append(q)
 
 
 list_polishing_function()
@@ -48,16 +52,24 @@ stock_price_g = []
 inv_g = []
 Bankroll = 100
 volatility_strategy_bankroll_g = [100]
-buy_and_hold_strategy_bankroll_g = []
+
 
 def profitability_testing_function(Bankroll: int):
+    total_count = 0
+    buy_and_hold_bankroll = 100
+    buy_and_hold_inv_ratio = polished_price_list[0] / buy_and_hold_bankroll
+    buy_and_hold_strategy_bankroll_g = [100]
     for i in polished_price_list:
+        buy_and_hold_strategy_bankroll_g.append(i/ buy_and_hold_inv_ratio)
+        total_count += 1
         if len(inv_g) == 0:
             inv_g.append(1)
             stock_price_g.append(i)
             Bankroll -= 1
-            print("111iii: ", i)
-            print("111Bankroll: ", Bankroll)
+            # print("111iii: ", i)
+            # print("111Bankroll: ", Bankroll)
+            # print("1111stock_price_g: ", stock_price_g)
+            # print("1111polished_price_list: ", polished_price_list)
             volatility_strategy_bankroll_g.append(Bankroll)
             print("")
         elif len(inv_g) > 0:
@@ -65,42 +77,56 @@ def profitability_testing_function(Bankroll: int):
                 inv_g.append(inv_g[- 1] * ratio_drop)
                 Bankroll -= inv_g[- 1] * ratio_drop
                 stock_price_g.append(stock_price_g[- 1] * ratio_drop)
-                print("2222iii: ", i)
-                print("222Bankroll: ", Bankroll)
+                # print("2222iii: ", i)
+                # print("222Bankroll: ", Bankroll)
                 volatility_strategy_bankroll_g.append(Bankroll)
                 print("")
+                if i < stock_price_g[- 1] * ratio_drop and total_count < len(polished_price_list):
+                    polished_price_list[total_count] = i
+
             elif i >= stock_price_g[- 1] * ratio_increase:
-                print("3333iii: ", i)
-                print("333111inv_g: ", inv_g)
+                # print("3333iii: ", i)
+                # print("333111inv_g: ", inv_g)
+                # print("333stock_price_g: ", stock_price_g)
+                # print("3333polished_price_list: ", polished_price_list)
                 Bankroll += inv_g[- 1] * ratio_increase
                 stock_price_g.append(stock_price_g[- 1] * ratio_increase)
                 del stock_price_g[- 1]
                 del inv_g[- 1]
-                print("333Bankroll: ", Bankroll)
+                # print("333Bankroll: ", Bankroll)
+                # print("333stock_price_g: ", stock_price_g)
                 volatility_strategy_bankroll_g.append(Bankroll)
-                print("333inv_g: ", inv_g)
+                # print("333inv_g: ", inv_g)
                 if len(inv_g) == 0:
                     inv_g.append(1)
                     stock_price_g.append(i)
                     Bankroll -= 1
-                print("3fff33Bankroll: ", Bankroll)
-                volatility_strategy_bankroll_g.append(Bankroll)
-                print("3fff33inv_g: ", inv_g)
+                    volatility_strategy_bankroll_g.append(Bankroll)
+                # print("3fff33Bankroll: ", Bankroll)
+                # print("3333stock_price_g: ", stock_price_g)
+                # print("3fff33inv_g: ", inv_g)
                 print("")
+                if i > stock_price_g[- 1] * ratio_drop and total_count < len(polished_price_list):
+                    polished_price_list[total_count] = i
+                    # print("333eeepolished_price_list: ", polished_price_list)
 
-    return Bankroll
+    return [Bankroll, buy_and_hold_strategy_bankroll_g]
 
 
-Bankroll = profitability_testing_function(Bankroll)
-print("Bankroll: ", Bankroll)
+func_list = profitability_testing_function(Bankroll)
+Bankroll = func_list[0]
+buy_and_hold_strategy_bankroll_g = func_list[1]
+print("len(buy_and_hold_strategy_bankroll_g): ", len(buy_and_hold_strategy_bankroll_g))
+print("len(volatility_strategy_bankroll_g): ", len(volatility_strategy_bankroll_g))
 
 def graph_plotting_function():
     print("plotting begins")
     plt.figure(figsize=(10, 5), dpi=350)  # High DPI is used for sharpness
     plt.figure(figsize=(10, 5))
-    plt.plot(list(range(1, len(volatility_strategy_bankroll_g) + 1)), volatility_strategy_bankroll_g, marker='o', linestyle='-', color='red', label='Bankroll', linewidth=1, alpha=1, antialiased=False)
-    #plt.plot(list(range(1, len(polished_price_list) + 1)), polished_price_list, marker='o', linestyle='-', color='blue', label='Bankroll', linewidth=1, alpha=1, antialiased=False)
-    #plt.plot(list(range(1, len(buy_and_hold_strategy_bankroll_g) + 1)), buy_and_hold_strategy_bankroll_g, marker='o', linestyle='-', color='blue', label='Bankroll', linewidth=1, alpha=1, antialiased=False)
+    plt.plot(list(range(1, len(volatility_strategy_bankroll_g) + 1)), volatility_strategy_bankroll_g, marker='o',
+             linestyle='-', color='red', label='volatility strategy bankroll', linewidth=1, alpha=1, antialiased=False)
+    # plt.plot(list(range(1, len(polished_price_list) + 1)), polished_price_list, marker='o', linestyle='-', color='blue', label='Bankroll', linewidth=1, alpha=1, antialiased=False)
+    # plt.plot(list(range(1, len(buy_and_hold_strategy_bankroll_g) + 1)), buy_and_hold_strategy_bankroll_g, marker='o', linestyle='-', color='blue', label='buy and hold strategy bankroll', linewidth=1, alpha=1, antialiased=False)
 
     plt.title('Bankroll vs. Number of Games')
     plt.xlabel('Number of Games')
